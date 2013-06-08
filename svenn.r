385cd9abc84c9010de22a77f6b730feb044200fc
kverhun <k.verhun@gmail.com>
2013-06-09 01:27:29 +0300

    svenn: added return after last step
    tested :
    - (100-x)^2, x0 = 30, delta = 5
    gives (65, 145) instead of (65, 185)
    - cosh(x), x0 = 30, delta = 5
    gives (-25,15) instead of (-45, 15)

--- svenn.r
+++ svenn.r
@@ -14,7 +14,24 @@ svenn <- function (f, x0, delta)
     f3 <- f(x3)
     if (f1 >= f2 && f2 <= f3)
     {
-      return (c(x1,x3))
+      if (step > 0)
+      {
+        x4 <- x3 - step/2
+        f4 <- f(x4)
+        if (f4 > f2)
+          return (c(x1,x4))
+        else
+          return (c(x2,x3))
+      }
+      else
+      {
+        x4 <- x1 - step/2
+        f4 <- f(x4)
+        if (f4 > f2)
+          return (c(x4,x3))
+        else
+          return (c(x1,x3))
+      }
     }
     if (f1 >= f2 && f2 >= f3)
     {
